661a9aeb7efa5e3ca850fbebc891bf3a79074230
PabloH2022 <pabllohenrique08@gmail.com>
2023-01-06 00:32:58 -0300

    Initial commit

---
+++ aula11/ex010.js
@@ -0,0 +1,8 @@
+var vel = 40
+console.log('A velocidade do seu carro é de '+vel+'Km/h')
+if(vel > 60){//Codição simples
+    console.log('Você ultrapassou o limite de velocidade! MULTADO!')
+} 
+    console.log('Dirija sempre com o cinto de segurança!')
+
+
