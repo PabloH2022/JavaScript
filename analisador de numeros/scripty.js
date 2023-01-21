33f08fb1b85f568107431934b1889d673df00f6a
PabloH2022 <pabllohenrique08@gmail.com>
2023-01-21 20:06:28 -0300

    Update scripty.js

--- analisador de numeros/scripty.js
+++ analisador de numeros/scripty.js
@@ -1,19 +1,23 @@
     let valor = document.getElementById('txtc')
     let resp = document.getElementById('resposta')
-    let numero = [valor.value]
+    let numero = []
+    numero.push(valor.value)
 
 function adicionar() {
     
      if (valor.value.length == 0) {
         window.alert('[ERRO] existem campos vazios!')
      } else {
+        for(let pos in numero) {
+            resp.innerHTML = (pos[numero[0]])
+        }
         
         for(let pos = 0; pos<numero.length; pos++) {
             resp.innerHTML = ('O Valor ' +numero[pos]+ ' Foi adicionado')
         } 
         return numeros
     } 
-    let numeros = []
+    
         
 
 
