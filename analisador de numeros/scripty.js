e1ffb6189678ebe5997a2099c93136fd6d110820
PabloH2022 <pabllohenrique08@gmail.com>
2023-01-29 11:56:31 -0300

    atualizado

--- analisador de numeros/scripty.js
+++ analisador de numeros/scripty.js
@@ -1,5 +1,5 @@
 let num = document.getElementById('txtc')
-let lista = document.getElementsByTagName('flista')
+let lista = document.getElementById('flista')
 let res = document.getElementById('res')
 let valores = []
 
@@ -27,13 +27,45 @@ function adicionar() {
     if (inNumero(num.value) &&  !inLista(num.value, valores)) {
         
         valores.push(Number(num.value))
-
         let item = document.createElement('option')
-        item.text = ('O valor '+(num.value)+ ' Adicionado')
+        item.text = ('Valor ' + num.value +' foi adicionado')
         lista.appendChild(item)
-
+        res.innerHTML = ''
     } else {
         window.alert('Valor inválido ou já encontrado na Lista!')
     }
+    num.value = ''
+    num.focus()
 
+}
+
+function finalizar () {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar')
+    } else {
+        let tot = valores.length
+        let ordem = valores.sort()
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores) {
+
+            soma+= valores[pos]
+
+            if (valores[pos] > maior)
+            maior = valores[pos]
+            if (valores[pos] < menor)
+            menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += ('<p>Ao todo, temos ' +tot+ ' números cadastrados. </p>')
+        res.innerHTML += ('<p> os números em ordem crescente são ' +ordem+ '</p>')
+        res.innerHTML += ('<p> O maior numero é o '+maior+ '</p>')
+        res.innerHTML += ('<p> O menor numero é o ' +menor+ '</p>')
+        res.innerHTML += ('<p> Somando todos os valores temos ' +soma+'</p>')
+        res.innerHTML += ('<p> A média entre os valores digitados é de ' +media+ '</p>')
+
+    }
 }
