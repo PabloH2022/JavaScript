6b4e9858f27f122ca88c4869108880bcf514adc0
PabloH2022 <pabllohenrique08@gmail.com>
2023-01-31 14:36:34 -0300

    feito adicao a caixa de texto

--- SITE-RIFA/script.js
+++ SITE-RIFA/script.js
@@ -1,15 +1,44 @@
 let res = window.document.getElementById('caixabilhete')
+let numero = 0
+let display = document.getElementById('finalizar').style.display
 
-function adicionar1(init) {
-    var count = init || 0
-    for (let c = 0; count < in) {
-        count++;
-        res.innerHTML = (count)
-    }
+function mais() {
+    numero = numero + 1
+    res.innerHTML = numero
+    res.innerHTML = style.display = "none"
+
+}
+function menos() {
+    if(numero == 0) {
+        numero = 0
+    } else {
+        numero = numero - 1
+        res.innerHTML = numero
+    }  
+
+}
+
+function adicionar1() {
+    numero = numero + 1
+    res.innerHTML = numero
 }
 
 function adicionar30() {
-    res.innerHTML = '30' 
+     numero = numero + 30
+     res.innerHTML = numero
     
      
+}
+function adicionar50() {
+    numero = numero + 50
+    res.innerHTML = numero
+}
+
+function adicionar100() {
+    numero = numero + 100
+    res.innerHTML = numero
+}
+
+function finalizar() {
+    
 }
