b02757a81b537a3093fe9a945b40fe09f2161bd5
PabloH2022 <pabllohenrique08@gmail.com>
2023-01-31 12:49:35 -0300

    atu

--- SITE-RIFA/script.js
+++ SITE-RIFA/script.js
@@ -1,17 +1,15 @@
 let res = window.document.getElementById('caixabilhete')
-function adicionar1() {
-    let adicao = + 1
-    res.innerHTML = adicao
-    
 
-
-
-    
-    
-
-
+function adicionar1(init) {
+    var count = init || 0
+    for (let c = 0; count < in) {
+        count++;
+        res.innerHTML = (count)
+    }
 }
 
 function adicionar30() {
+    res.innerHTML = '30' 
+    
      
 }
