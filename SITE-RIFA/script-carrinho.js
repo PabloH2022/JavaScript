42d38fbdf81ba921c6afefb2fa967d6162da40fa
PabloH2022 <pabllohenrique08@gmail.com>
2023-02-01 12:15:46 -0300

    comm

---
+++ SITE-RIFA/script-carrinho.js
@@ -0,0 +1,6 @@
+let nome = document.getElementById('nameusu')
+let tel = documernt.getElementById('telusu')
+let infor = document.getElementById('informações')
+
+infor.innerHTML = (nome)
+infor.innerHTML += (tel)
