1f0cd5d0138859d3afc40b70ab30575631118fc5
PabloH2022 <pabllohenrique08@gmail.com>
2023-02-14 11:00:20 -0300

    commit

---
+++ GERENCIADOR DE TAREFAS/script.js
@@ -0,0 +1,16 @@
+let atual = document.getElementById('atual')
+let despesas = document.getElementById('despesas')
+let receitas = document.getElementById('receitas')
+let inputName = document.getElementById('inputName')
+let inputValor = document.getElementById('inputValor')
+
+let Name = localStorage.setItem('nome')
+
+let Nome = JSON.parse('nome', JSON.stringify(inputName))
+
+console.log(Nome)
+
+function adicionar() {
+    
+}
+
